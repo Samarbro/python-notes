fd031a955e02ebafeebb0d95ee69d71678872873
Samarbro <perposemulti@gmail.com>
2025-11-13 16:32:02 +0530

    Add files via upload

---
+++ python/Chapter 1/Modules, comments & pip .py
@@ -0,0 +1,38 @@
+# MODULES 
+# A module is a file containing code written by somebody else (usually) which can be imported and used in our programs.
+
+
+
+# PIP 
+
+# pip is the package maneger for python. you can use pip to install a module on your system. 
+
+# use this command in terminal..
+
+# pip install flask 
+
+
+
+
+# types of modules 
+
+# there are two types of moodules 
+
+# 1.  Build in module (preinstalled in python)
+# 2. external module (need to install by using pip)
+
+# comments i writted all thing by using '#' means comments python will ignore ths
+
+
+# comment types 
+
+""" 1. is single line comment 
+2. is multiline comments"""
+
+
+# this is single line comment through "#"
+
+""" this is 
+multiline comments
+by using triple dots
+in starting or ending"""

5c7853628e402be851ebcf2dd25cb0a1f9ff175e
Samarbro <perposemulti@gmail.com>
2025-11-24 16:39:49 +0530

    Add files via upload

---
+++ Chapter 3/1 Strings.py
@@ -0,0 +1,18 @@
+# String is a data type in python.
+# String is a sequence of characters enclosed in quotes.
+# We can primarily write a string in these three ways.
+# a ='harry' # Single quoted string
+#  b = "harry" # Double quoted string
+#   c = '''harry''' # Triple quoted string STRING SLICING
+# A string in python can be sliced for getting a part of the strings.
+# Consider the following string:
+
+
+# the rule of of slicing strings data
+
+name = "harry" 
+
+nameshort = name[0:3] # start from index 0 all the way till 3 (exlcuding 3)
+
+print (nameshort)
+

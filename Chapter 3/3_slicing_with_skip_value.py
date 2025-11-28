2fbd25491ccb1fb353d288308b0d4494e21edca6
Samarbro <perposemulti@gmail.com>
2025-11-28 16:24:09 +0530

    Add files via upload

---
+++ Chapter 3/3_slicing_with_skip_value.py
@@ -0,0 +1,9 @@
+# slice with skip value 
+
+# we can provide a skip value as a part of our slice like this
+
+word = "amazing"
+
+word2 = [1: 6: 2] # "mzn"
+
+print(word2)
---
+++ Chapter 3/5_escape sequence.py
@@ -0,0 +1,17 @@
+a = " I'm taller then you\n you \"think\""
+
+print (a)
+
+
+# ESCAPE SEQUENCE CHARACTERS 
+
+""" help us to do changes in string 
+
+for example if you add  \n    anywhere in the string, 
+string will change in new line 
+
+
+so you can use more escape sequence characters to modify string
+
+
+you can also find some escape sequence in the internet like " chatgpt" """

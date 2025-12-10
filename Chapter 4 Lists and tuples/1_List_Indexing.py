a75116b04122fd73a7414e93705551c71e65fab4
Samar vishwakarma <perposemulti@gmail.com>
2025-12-10 16:36:54 +0530

    Add files via upload

---
+++ Chapter 4 Lists and tuples/1_List_Indexing.py
@@ -0,0 +1,8 @@
+# introduction python list container
+
+''' Python lists are containers to store a set of value of any 
+data type. '''
+
+friends = ["apple","akash","rohan",7,False]
+
+print(friends)
---
+++ Chapter 4 Lists and tuples/3_list method.py
@@ -0,0 +1,16 @@
+# consider the following list:
+
+11 = [1,8,7,2,21,15]
+
+'''
+
+11.sort():updates the list to [1,2,7,8,15,21]
+11.reverse(): updates the list to [15,21,2,7,8,1]
+11.append(8): adds 8 at the end of the list
+11.insert(3,8): this will add 8 at 3 index 
+11.pop(2):will delete element at index 2 and return its value.
+11.remove(21):will remove 21 from the list.
+
+'''
+
+
